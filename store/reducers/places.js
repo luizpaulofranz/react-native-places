b98a611e4377abd3674bebd7026b5fdf758a8f19
luizpaulofranz <luizpaulofranz@gmail.com>
2018-10-17 17:09:26 -0300

    Starts adding REDUX on project.

---
+++ store/reducers/places.js
@@ -0,0 +1,13 @@
+const initialState = {
+    places: [],
+    selectedPlace: null
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action) {
+        default:
+            return state;
+    }
+}
+
+export default reducer;
